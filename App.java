e1e5d6ab9566d1a2c43d86db64783f3128a23427
satya <satyajitkhuntia122333@gmail.com>
2024-09-10 19:45:26 +0530

    Bug 02

--- App.java
+++ App.java
@@ -1,7 +1,13 @@
 class App{
     public static void main(String[] args){
+<<<<<<< Updated upstream
 
         //Bug - 02
+=======
+        
+        //Bug - 01
+        System.out.println(
+>>>>>>> Stashed changes
         System.out.println("Hello World - develope branch");
     }
 }
