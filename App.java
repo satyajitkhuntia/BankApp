fa2b175b7c4a702d4886e331b811c180d0e687ed
satya <satyajitkhuntia122333@gmail.com>
2024-09-10 20:08:25 +0530

    erge with develope

--- App.java
+++ App.java
@@ -1,13 +1,10 @@
 class App{
     public static void main(String[] args){
-<<<<<<< Updated upstream
 
         //Bug - 02
-=======
         
         //Bug - 01
         System.out.println(
->>>>>>> Stashed changes
         System.out.println("Hello World - develope branch");
     }
 }
